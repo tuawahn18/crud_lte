3251e8754a5094e950f6a669791e7bfecce7225d
tuawahn18 <Tuaasnanh188@gmail.com>
2024-08-02 13:33:33 +0700

    crud_adminlte

---
+++ crud_lte/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController; 
+use App\Http\Controllers\PermissionsController;
+use App\Http\Controllers\RolesController;
+use App\Http\Controllers\UsersController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth', 'permission']], function(){
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');    
+    Route::group(['prefix'=> 'users', 'as' => 'users.'], function(){
+        Route::resource('permissions', PermissionsController::class);
+        Route::resource('roles', RolesController::class);
+    });    
+    Route::resource('users', UsersController::class); 
+});
